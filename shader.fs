02ded05897956ca757a9b654ef8ae0b7b8ae3a82
ishbosamiya <ishbosamiya@gmail.com>
2020-01-13 17:52:17 +0530

    glsl: basic diffuse lighting

--- shader.fs
+++ shader.fs
@@ -1,10 +1,24 @@
 #version 330 core
 out vec4 FragColor;
 
-in vec2 TexCoord;
+in vec3 TexCoord;
+in vec3 Normal;
+in vec3 FragPos;
 
 void main()
 {
-  FragColor = vec4(0.0f, 0.0f, 0.9f, 1.0f);
-}
+  vec3 light_color = vec3(1.0, 1.0, 1.0);
+  vec3 object_color = vec3(1.0, 0.5, 0.31);
+  vec3 light_pos = vec3(1.2, 3.0, 3.0);
 
+  float ambient_strength = 0.1;
+  vec3 ambient = ambient_strength * light_color;
+
+  vec3 norm = normalize(Normal);
+  vec3 light_dir = normalize(light_pos - FragPos);
+  float diff = max(dot(norm, light_dir), 0.0);
+  vec3 diffuse = diff * light_color;
+
+  vec3 result = (ambient + diffuse) * object_color;
+  FragColor = vec4(result, 1.0);
+}
